688b092c54ad2d310d0be57161ed930a25967b05
tangzhihui2024 <tangzhihui@modernauto.cn>
2024-08-22 16:22:41 +0800

    测试版本

--- ruoyi-ui/src/utils/request.js
+++ ruoyi-ui/src/utils/request.js
@@ -11,6 +11,8 @@ let downloadLoadingInstance;
 // 是否显示重新登录
 export let isRelogin = { show: false };
 
+
+
 axios.defaults.headers['Content-Type'] = 'application/json;charset=utf-8'
 // 创建axios实例
 const service = axios.create({
