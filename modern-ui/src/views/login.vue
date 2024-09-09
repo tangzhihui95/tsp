649dd89f0dd137b445da5805518ba4edb4d2f89e
tangzhihui95 <13122802551@163.com>
2024-09-09 10:28:09 +0800

    标题替换

--- modern-ui/src/views/login.vue
+++ modern-ui/src/views/login.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="login">
     <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form">
-      <h3 class="title">若依后台管理系统测试</h3>
+      <h3 class="title">摩登车联网服务平台</h3>
       <el-form-item prop="username">
         <el-input
           v-model="loginForm.username"
