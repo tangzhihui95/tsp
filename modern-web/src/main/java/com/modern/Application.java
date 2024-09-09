cd5e4d690d36f00766f1e2819f4b1070e75e8520
tangzhihui95 <13122802551@163.com>
2024-09-09 13:16:12 +0800

    web启动模块名称变更

--- modern-web/src/main/java/com/modern/Application.java
+++ modern-web/src/main/java/com/modern/Application.java
@@ -17,7 +17,7 @@ public class Application {
 
         // System.setProperty("spring.devtools.restart.enabled", "false");
         SpringApplication.run(Application.class, args);
-        System.out.println("(♥◠‿◠)ﾉﾞ  Tsp平台启动成功   ლ(´ڡ`ლ)ﾞ  \n");
+        System.out.println("(♥◠‿◠)ﾉﾞ  Tsp-平台启动成功   ლ(´ڡ`ლ)ﾞ  \n");
 
        /* System.out.println("(♥◠‿◠)ﾉﾞ  启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
         System.out.println("(♥◠‿◠)ﾉﾞ  启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
