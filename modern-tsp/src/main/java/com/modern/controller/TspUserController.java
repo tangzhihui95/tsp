a9e18bd05792946bf4d2344df6a546cb97e0f552
tangzhihui95 <13122802551@163.com>
2025-09-03 13:45:25 +0800

    TSP用户开发-分页、导入、导出

--- modern-tsp/src/main/java/com/modern/controller/TspUserController.java
+++ modern-tsp/src/main/java/com/modern/controller/TspUserController.java
@@ -5,9 +5,11 @@ import com.modern.common.core.domain.Result;
 import com.modern.common.core.page.PageInfo;
 import com.modern.common.enums.BusinessType;
 import com.modern.common.utils.JsonResult;
+import com.modern.common.utils.poi.ExcelUtil;
 import com.modern.model.dto.TspUserDTO;
 import com.modern.model.dto.TspUserPageListDTO;
 import com.modern.model.vo.TspUserAddVO;
+import com.modern.model.vo.TspUserExcelVO;
 import com.modern.model.vo.TspUserPageListVO;
 import com.modern.service.TspUserService;
 import io.swagger.annotations.Api;
@@ -17,8 +19,10 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
@@ -63,7 +67,7 @@ public class TspUserController {
 
     @ApiOperation("一般用户-用户详情")
     @ApiImplicitParams(value = {
-            @ApiImplicitParam(name = "tspUserId",value = "用户ID",dataType = "Long",required = false),
+            @ApiImplicitParam(name = "tspUserId", value = "用户ID", dataType = "Long", required = false),
     })
     @GetMapping({"/get/{tspUserId}"})
     public Result<TspUserDTO> get(@PathVariable("tspUserId") Long tspUserId) {
@@ -72,7 +76,7 @@ public class TspUserController {
 
     @ApiOperation("一般用户-当前车辆详情")
     @ApiImplicitParams(value = {
-            @ApiImplicitParam(name = "tspUserId",value = "用户ID",dataType = "Long",required = false),
+            @ApiImplicitParam(name = "tspUserId", value = "用户ID", dataType = "Long", required = false),
     })
     @GetMapping({"/find/{tspUserId}"})
     public Result<List<Map<String, Object>>> findCarInfo(@PathVariable("tspUserId") Long tspUserId) {
@@ -81,7 +85,7 @@ public class TspUserController {
 
     @ApiOperation("一般用户-历史绑定记录")
     @ApiImplicitParams(value = {
-            @ApiImplicitParam(name = "tspUserId",value = "用户ID",dataType = "Long",required = false),
+            @ApiImplicitParam(name = "tspUserId", value = "用户ID", dataType = "Long", required = false),
     })
     @GetMapping({"/findHistory/{tspUserId}"})
     public Result<List<Map<String, Object>>> findHistory(@PathVariable("tspUserId") Long tspUserId) {
@@ -92,10 +96,36 @@ public class TspUserController {
     @Log(title = "一般用户-删除", businessType = BusinessType.DELETE)
     @ApiOperation("删除")
     @ApiImplicitParams(value = {
-            @ApiImplicitParam(name = "tspUserIds",value = "用户ID集合",dataType = "Long",required = false),
+            @ApiImplicitParam(name = "tspUserIds", value = "用户ID集合", dataType = "Long", required = false),
     })
     @DeleteMapping({"deletes/{tspUserIds}"})
     public Result deletes(@PathVariable("tspUserIds") Long[] tspUserIds) {
         return Result.ok(tspUserService.deletes(tspUserIds));
     }
+
+    @ApiOperation("一般用户-下载模版")
+    @GetMapping({"/importTemplate"})
+    public void importTemplate() {
+        ExcelUtil<TspUserExcelVO> util = new ExcelUtil(TspUserExcelVO.class);
+        util.importTemplateExcel("一般用户信息数据");
+    }
+
+    @ApiOperation("一般用户-导入")
+    @PreAuthorize("@ss.hasPermi('tsp:user:importUser')")
+    @Log(title = "- ", businessType = BusinessType.IMPORT)
+    @PostMapping({"/importUser"})
+    public Result importUser(MultipartFile file, Boolean isUpdateSupport) throws IOException {
+        return Result.ok(tspUserService.importUser(file, isUpdateSupport));
+    }
+
+    @ApiOperation("一般用户-导出")
+    @PreAuthorize("@ss.hasPermi('tsp:user:export')")
+    @Log(title = "- ", businessType = BusinessType.EXPORT)
+    @GetMapping({"/exportUser"})
+    public void export(TspUserPageListVO vo) {
+        List<TspUserPageListDTO> list = tspUserService.exportList(vo);
+        ExcelUtil<TspUserPageListDTO> util = new ExcelUtil(TspUserPageListDTO.class);
+        util.exportExcel(list, "一般用户信息数据");
+    }
+
 }
